5d0a6ea0f2bc6d5ea6b0300157ac0995cdc71dca
amr <amrashraf314@gmail.com>
2025-07-30 15:43:19 +0300

    edit

--- server.js
+++ server.js
@@ -5,7 +5,12 @@ const middlewares = jsonServer.defaults()
 const port = process.env.PORT || 3001
 
 server.use(middlewares)
-server.use(router)
+server.use(router);
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', '*')
+    next()
+  })
 server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`)
 })
